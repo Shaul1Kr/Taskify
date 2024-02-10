5d27a59710c23b7feabc737ee43b6522758e4440
Shauli Kraus <shaulikr07@gmail.com>
2024-02-10 15:22:23 +0200

    Added register and change password funcitons

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
 
-export default async function login(req: Request, res: Response) {
+export async function login(req: Request, res: Response) {
   try {
     const { username, password } = req.body;
     console.info(
@@ -25,5 +25,48 @@ export default async function login(req: Request, res: Response) {
   } catch (error) {
     console.error(error);
     return res.status(401).json({ message: "Authentication failed" });
+  }
+}
+
+export async function register(req: Request, res: Response) {
+  try {
+    const { firstName, lastName, username, password } = req.body;
+    console.log(
+      `Trying to register for a new user with first name: ${firstName} last name: ${lastName} user name: ${username} and password: ${password}`
+    );
+    const salt = await bcrypt.genSalt();
+    const saltPassword = await bcrypt.hash(password, salt);
+    await User.create({
+      firstName,
+      lastName,
+      username,
+      password: saltPassword,
+    });
+    return res.status(200).send("Register successfully");
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: " Bed Request" });
+  }
+}
+
+export async function changePassword(req: Request, res: Response) {
+  try {
+    const { username, password, newPassword } = req.body;
+    console.log(
+      `Trying to change password with user name: ${username} password: ${password} with new password ${newPassword}`
+    );
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.status(403).json({ msg: "email or password incorrect" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(403).json({ msg: "email or password incorrect" });
+    const salt = await bcrypt.genSalt();
+    const newSaltPassword = await bcrypt.hash(newPassword, salt);
+    await User.findOneAndUpdate({ username }, { password: newSaltPassword });
+    return res.status(200).send("Password as been changed");
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: " Bed Request" });
   }
 }
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import { changePassword, login, register } from "../controllers/auth";
 const router = express.Router();
+
+router.post("/login", login);
+router.post("/register", register);
+router.post("/changePassword", changePassword);
 
 export default router;
