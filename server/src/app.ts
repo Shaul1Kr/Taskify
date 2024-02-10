70850e3d438ef1757a97ec3ac9f002f09edf8ba1
Shauli Kraus <shaulikr07@gmail.com>
2024-02-10 15:21:55 +0200

    Using api data and db name

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,17 +3,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import api from "./routes/api";
+import data from "./data/users";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
+app.use("/api", api);
 
 const PORT = process.env.PORT;
+const dbName = process.env.DBNAME;
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, { dbName })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
     //Insert users to db
