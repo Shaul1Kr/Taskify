217053defb2a86758978c8dbf4d35d30fa1ca442
Shauli Kraus <shaulikr07@gmail.com>
2024-02-12 12:50:08 +0200

    Finished all the api's for tasks

--- server/src/controllers/task.ts
+++ server/src/controllers/task.ts
@@ -11,3 +11,76 @@ export async function getTasks(req: Request, res: Response) {
     return res.status(401).json({ message: " Bed Request" });
   }
 }
+
+export async function createTask(req: Request, res: Response) {
+  try {
+    const {
+      title,
+      description,
+      dueDate,
+      priority,
+      status,
+      assignee,
+      createdBy,
+    } = req.body;
+    console.log(`Creating new task by ${createdBy}`);
+    await Task.create({
+      title,
+      description,
+      dueDate,
+      priority,
+      status,
+      assignee,
+      createdBy,
+    });
+    return res.status(200).send(`The task created successfully`);
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: " Bed Request" });
+  }
+}
+
+export async function getTask(req: Request, res: Response) {
+  try {
+    const { taskid } = req.params;
+    console.log(`Getting task with task id: ${taskid}`);
+    const task = await Task.findById(taskid);
+    return res.status(200).json({ task });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: " Bed Request" });
+  }
+}
+
+export async function updateTask(req: Request, res: Response) {
+  try {
+    const { taskid } = req.params;
+    const { title, description, dueDate, priority, status, assignee } =
+      req.body;
+    console.log(`Update task by task id: ${taskid}`);
+    await Task.findByIdAndUpdate(taskid, {
+      title,
+      description,
+      dueDate,
+      priority,
+      status,
+      assignee,
+    });
+    return res.status(200).send(`The task updated successfully`);
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: " Bed Request" });
+  }
+}
+
+export async function deleteTask(req: Request, res: Response) {
+  try {
+    const { taskid } = req.params;
+    console.log(`Delete task with task id: ${taskid}`);
+    await Task.findByIdAndDelete(taskid);
+    return res.status(200).send(`The task deleted successfully`);
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: " Bed Request" });
+  }
+}
--- server/src/routes/task.ts
+++ server/src/routes/task.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { getTasks } from "../controllers/task";
+import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task";
 const router = express.Router();
 
 router.get("/getTasks", getTasks);
+router.post("/createTask", createTask);
+router.get("/getTask/:taskid", getTask);
+router.put("/updateTask/:taskid", updateTask);
+router.delete("/deleteTask/:taskid", deleteTask);
 
 export default router;
