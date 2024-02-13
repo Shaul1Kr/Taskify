6a65f7239d046cb940a9bdf7fc2e5ad8dec64b70
Shauli Kraus <shaulikr07@gmail.com>
2024-02-13 19:54:00 +0200

    added new apis

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -2,11 +2,13 @@ import express from "express";
 import auth from "./auth";
 import task from "./task";
 import user from "./user";
+import search from "./search";
 
 const router = express.Router();
 
 router.use("/auth", auth);
 router.use("/task", task);
 router.use("/user", user);
+router.use("/serach", search);
 
 export default router;
