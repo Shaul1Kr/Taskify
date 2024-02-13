17a560bbbe1e935d46a585b88e3e35fc800149bd
Shauli Kraus <shaulikr07@gmail.com>
2024-02-13 19:53:53 +0200

    Added user interface

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export interface IGetUserAuthInfoRequest extends Request {
+  user: string;
+}
+
+export default function verifyToken(
+  req: IGetUserAuthInfoRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.cookies["access_token"];
+  if (!token) {
+    return res.status(403).json({ message: "Token not provided" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Token verification failed" });
+    }
+    req.user = decoded;
+    next();
+  });
+}
