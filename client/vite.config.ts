d0fc686bdeac72d87a7627e2720603e78c098064
Shaul1Kr <shaulikr07@gmail.com>
2024-06-02 15:57:35 +0300

    Change the request urls

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://taskify-dr0v.onrender.com",
         changeOrigin: true,
         secure: false,
       },
